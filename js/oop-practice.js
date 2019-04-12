a7e12ab73cdcfa85690261b008af3510b3a311ca
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-11 19:17:25 -0700

    oop-practice.js: Complete Step 03

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -13,3 +13,13 @@ var Dog = function () {
 var cat, dog;
 cat = new Cat();
 dog = new Dog();
+
+// STEP 3
+function Animal() {
+    "use strict";
+    this.display = function () {
+        window.console.log("The Animal has been created");
+    };
+}
+var animal = new Animal();
+animal.display();
