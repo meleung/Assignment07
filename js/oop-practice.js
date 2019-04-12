36f9478bb212608785272b7b4dc4e7479ae14e71
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-11 19:20:35 -0700

    oop-practice.js: Complete Step 04

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,25 +1,36 @@
 /*eslint-env browser*/
 
 // STEP 1
-function Cat() {
-    "use strict";
-}
-
-var Dog = function () {
-    "use strict";
-};
+//function Cat() {
+//    "use strict";
+//}
+//
+//var Dog = function () {
+//    "use strict";
+//};
 
 // STEP 2
-var cat, dog;
-cat = new Cat();
-dog = new Dog();
+//var cat, dog;
+//cat = new Cat();
+//dog = new Dog();
 
 // STEP 3
-function Animal() {
-    "use strict";
-    this.display = function () {
-        window.console.log("The Animal has been created");
-    };
-}
-var animal = new Animal();
-animal.display();
+//function Animal() {
+//    "use strict";
+//    this.display = function () {
+//        window.console.log("The Animal has been created");
+//    };
+//}
+//var animal = new Animal();
+//animal.display();
+
+// STEP 4
+//function Animal(msg) {
+//    "use strict";
+//    this.msg = msg;
+//    this.display = function () {
+//        window.console.log(this.msg);
+//    };
+//}
+//var animal = new Animal("The Animal has been created");
+//animal.display();
